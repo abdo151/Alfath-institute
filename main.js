49ddabaecb432b64a6107039b57772e4496b8bf1
abdo151 <abdologest8@gmail.com>
2023-11-24 02:25:14 +0200

    last

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //start header
 let menu = document.querySelector(".header .container .links ul");
 console.log(menu);
-let icons = document.querySelector(".header .container .links");
+let icons = document.querySelector(".header .container .links .icons");
 console.log(icons);
 function showAndHidden(e) {
   e.target.classList.toggle("hidden");
 }
 
-icons.addEventListener("click",_=> {
-    menu.classList.toggle("hidden");
+icons.addEventListener("click", (_) => {
+  menu.classList.toggle("hidden");
 });
 //end header
