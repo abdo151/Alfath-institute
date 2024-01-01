15dd9d1198508dd2882c3cd4c53efbab78d46caa
abdo151 <abdologest8@gmail.com>
2024-01-02 01:47:12 +0200

    last updates 1 january 24

--- main.js
+++ main.js
@@ -39,3 +39,50 @@ function change_background() {
 }
 change_background();
 //end landing random slider
+
+// start our staff
+const counters = document.querySelectorAll(".number");
+const speed = 200; // The lower the slower
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // Lower inc to slow and higher to speed up
+    const inc = target / speed;
+
+    // Check if target is reached
+    if (count < target) {
+      // Add inc to count and output in counter
+      counter.innerText = Math.ceil(count + inc);
+      // Call function every ms
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  const handleScroll = () => {
+    const statsPosition = counter.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if (statsPosition < screenPosition) {
+      updateCount();
+      window.removeEventListener("scroll", handleScroll);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+});
+
+// end our staff
+// start FAQ
+document.querySelectorAll('.faq-question').forEach(question => {
+  question.addEventListener('click', function() {
+    var faqItem = this.parentNode;
+    faqItem.classList.toggle('active');
+  });
+});
+
+// end FAQ
